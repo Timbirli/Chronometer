8e1eec0adbf3be0abf3be58e1c71d5e18b364ce0
reira <danilova1267@icloud.com>
2026-09-15 09:33:16 +0700

    Описание переменных из разметки в коде и функция форматирования времени

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvTimer: TextView
+    private lateinit var btnStart: Button
+    private lateinit var btnPause: Button
+    private lateinit var btnReset: Button
+
+    private val viewModel: StopwatchViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvTimer = findViewById(R.id.tvTimer)
+        btnStart = findViewById(R.id.btnStart)
+        btnPause = findViewById(R.id.btnPause)
+        btnReset = findViewById(R.id.btnReset)
+
+    }
+
+    // Форматирование из миллисекунд в строковый формат
+    private fun formatTime(millis: Long): String {
+        val seconds = (millis / 1000) % 60
+        val minutes = (millis / (1000 * 60)) % 60
+        val hours = (millis / (1000 * 60 * 60))
+
+        return if (hours > 0) {
+            String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
         }
     }
 }
